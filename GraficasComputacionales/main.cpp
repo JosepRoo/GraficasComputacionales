de3a43456aaaa10fbcc109fd856d13201e2b3cc1
JosepRoo <josepromll@gmail.com>
2017-09-04 18:46:43 -0500

    Examen

--- GraficasComputacionales/main.cpp
+++ GraficasComputacionales/main.cpp
@@ -13,7 +13,6 @@ void Initialize()
     std::vector<glm::vec2> positions;
     positions.push_back(glm::vec2(0.0f, 0.0f));
     for ( float i = 0.0f;i<360;i++ ) {
-
         positions.push_back(glm::vec2(glm::cos(glm::radians(i)),glm::sin(glm::radians(i))));
     }
     // Arreglo de colores en el cpu
@@ -29,8 +28,7 @@ void Initialize()
     GLuint positionsVBO;
     glGenBuffers(1, &positionsVBO);
     glBindBuffer(GL_ARRAY_BUFFER, positionsVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec2) * positions.size(),
-                 positions.data(), GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec2) * positions.size(), positions.data(), GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -38,8 +36,7 @@ void Initialize()
     GLuint colorsVBO;
     glGenBuffers(1, &colorsVBO);
     glBindBuffer(GL_ARRAY_BUFFER, colorsVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec3) * colors.size(),
-                 colors.data(), GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec3) * colors.size(), colors.data(), GL_STATIC_DRAW);
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -49,12 +46,9 @@ void Initialize()
     InputFile ifile;
 
     ifile.Read("DiscardCenter.vert");
-    GLuint vertexShaderHandle =
-            glCreateShader(GL_VERTEX_SHADER);
-    const GLchar *vertexSource_c =
-            (const GLchar*)vertexSource.c_str();
-    glShaderSource(vertexShaderHandle, 1,
-                   &vertexSource_c, nullptr);
+    GLuint vertexShaderHandle = glCreateShader(GL_VERTEX_SHADER);
+    const GLchar *vertexSource_c = (const GLchar*)vertexSource.c_str();
+    glShaderSource(vertexShaderHandle, 1, &vertexSource_c, nullptr);
     glCompileShader(vertexShaderHandle);
 
     ifile.Read("DiscardCenter.frag");
